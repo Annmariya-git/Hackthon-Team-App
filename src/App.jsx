264b97b4c2da68a0c97b9d9c2d575160b6f4b297
Annmariya-git <annmariyavarghese2027@gmail.com>
2026-07-15 20:53:36 +0530

    initialized

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddTeam from './components/AddTeam'
+import ViewTeam from './components/ViewTeam'
+import NavTeam from './components/NavTeam'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddTeam />}/>
+        <Route path='/ViewTeam' element={<ViewTeam />}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
